16b00351b85924c93c4d6cf48f50fb4b4f318819
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-04-09 14:20:57 +0000

    fixed font size of nav links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="bg-black text-light">
       <div className="container">
         <h1 className="display-1 py-3">iPhone 15 Pro</h1>
-        <ul className="nav">
+        <ul className="nav fs-5">
           <li className="nav-item">
             <NavLink to="/" className="nav-link  link-secondary">Overview</NavLink>
           </li>
